842771b24ad192aed1a497247c6d8a8ec9733cce
Tatiana Haddad <tatianadhaddad@gmail.com>
2019-07-07 13:25:01 -0700

    initial commit

---
+++ src/bookmarks/bookmarksRouter.js
@@ -0,0 +1,79 @@
+const express = require('express')
+const uuid = require("uuid/v4");
+const logger = require("../logger");
+const bookmarkRouter = express.Router();
+const bodyParser = express.json();
+const {bookmarks} = require('../bookmarks')
+
+bookmarkRouter
+  .route('/bookmarks')
+  .get((req, res) => {
+    res.json(bookmarks)
+  })
+  .post(bodyParser, (req, res) => {
+    const { title, url } = req.body;
+
+  if (!title) {
+    logger.error(`Title is required`);
+    return res.status(400).send("Invalid data");
+  }
+
+  if (!url) {
+    logger.error(`Url is required`);
+    return res.status(400).send("Invalid data");
+  }
+
+  const id = uuid();
+
+  const bookmark = {
+    id,
+    title, 
+    url
+  }
+
+  bookmarks.push(bookmark)
+
+  logger.info(`Bookmark with id ${id} created`);
+
+    res
+      .status(201)
+      .location(`http://localhost:8000/bookmarks/${id}`)
+      .json(bookmark);
+  });
+
+bookmarkRouter
+  .route('/bookmarks/:id')
+  .get((req, res) => {
+    const {id} = req.params
+    const bookmark = bookmarks.find(b => b.id == id)
+
+    if (!bookmark) {
+      logger.error(`Bookmark with id ${id} not found.`);
+      return res.status(404).send("Card Not Found");
+    }
+    res.json(bookmark);
+  })
+  .delete((req, res) => {
+    const {id} = req.params
+
+    const bookmarkIndex = bookmarks.findIndex(b => b.id == id)
+
+    if (bookmarkIndex === -1) {
+      logger.error(`Bookmark with id ${id} not found.`);
+      return res.status(404).send("Not found");
+    }
+
+    bookmarks.forEach(bookmark => {
+      const bookmarkId = bookmark.bookmarkId.filter(bid => bid !== id);
+      bookmark.bookmarkId = bookmarkId;
+    });
+
+    cards.splice(bookmarkIndex, 1);
+
+    logger.info(`Bookmark with id ${id} deleted.`);
+
+    res.status(204).end();
+
+  })
+
+module.exports = bookmarkRouter
